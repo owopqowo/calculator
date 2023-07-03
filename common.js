4e72d93eab11b4f66a29fa69dabcae56617b4a5c
KEBT\ij.won <ij.won@konai.com>
2023-07-03 16:17:03 +0900

    연산자 및 등호 선택 시 조건문 추가

--- common.js
+++ common.js
@@ -7,20 +7,36 @@ let operator = null;
 btns.addEventListener('click', (e) => {
   if(e.target.tagName !== 'BUTTON') return;
   if(e.target.dataset.type === 'reset') {
+    operator = null;
     inputCalc.value = 0;
     inputHistory.value = '';
   } else if(e.target.dataset.type === 'equal') {
     if(inputCalc.value == 0) return;
-    let resulte = inputCalc.value.split(operator);
-    inputCalc.value = calculate(Number(resulte[0]), Number(resulte[1]), operator);
-    inputHistory.value += '='
+    if(operator) {
+      let resulte = inputCalc.value.split(operator);
+      if (Number(resulte[1])) {
+        inputCalc.value = calculate(Number(resulte[0]), Number(resulte[1]), operator);
+        inputHistory.value += '=';
+      } else {
+        inputCalc.value = calculate(Number(resulte[0]), Number(resulte[0]), operator);
+        inputHistory.value += `${Number(resulte[0])}=`;
+      }
+      operator = null;
+    }
   } else {
     if(inputCalc.value == 0 && e.target.classList.contains('btn-operator')) return;
     if(inputCalc.value == 0) inputCalc.value = '';
-    inputCalc.value += e.target.dataset.type;
     if(e.target.classList.contains('btn-operator')) {
+      if(operator) {
+        console.log(operator);
+        inputCalc.value = inputCalc.value.replace(operator, e.target.dataset.type);
+        operator = e.target.dataset.type;
+        inputHistory.value = inputCalc.value;
+        return false;
+      }
       operator = e.target.dataset.type;
     } 
+    inputCalc.value += e.target.dataset.type;
     if(operator) {
       inputHistory.value = inputCalc.value;
     }
