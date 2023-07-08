78f3732dae0fa150f5e822a244c3ab5ef2b0c151
owopqowo <owo9370@gmail.com>
2023-07-08 17:25:42 +0900

    리팩토링

--- common.js
+++ common.js
@@ -3,36 +3,40 @@ const inputCalc = document.querySelector('.calculating');
 const btns = document.querySelector('.btns');
 
 let operator = null;
-const getMapKey = {
-  reset: () => {
-    resetCalculator();
-  },
-  equal: () => {
-    if(!operator) return;
-    let resulte = inputCalc.value.split(operator);
-    inputCalc.value = calculate(Number(resulte[0]), Number(resulte[1]) ?  Number(resulte[1]) :  Number(resulte[0]), operator);
-    inputHistory.value += `${Number(resulte[1]) ?  '' :  Number(resulte[0])}=`;
-    operator = null;
-  },
-  operator: (target) => {
-    if(inputCalc.value == 0) return;
-    operator ? inputCalc.value = inputCalc.value.replace(operator, target.dataset.operator) : inputCalc.value += target.dataset.operator;
-    inputHistory.value = inputCalc.value;
-    operator = target.dataset.operator;
-  },
-  number: (target) => {
-    if(inputCalc.value == 0) inputCalc.value = '';
-    inputCalc.value += target.dataset.number;
-    if(operator) {
-      inputHistory.value = inputCalc.value;
-    }
-  }
-}
 
 function resetCalculator() {
   operator = null;
   inputCalc.value = 0;
   inputHistory.value = '';
+}
+
+function equalCalculator() {
+  if (!operator) return;
+  const result = inputCalc.value.split(operator);
+  const resultFirst = +result[0];
+  const resultSecond = +result[1];
+  inputCalc.value = calculate(resultFirst, resultSecond ? resultSecond : resultFirst, operator);
+  inputHistory.value += resultSecond ? '=' : `${resultFirst}=`;
+  operator = null;
+}
+
+function operatorCalculator(newOperator) {
+  if (inputCalc.value == 0) return;
+  if (operator) {
+    inputCalc.value = inputCalc.value.replace(operator, newOperator);
+  } else {
+    inputCalc.value += newOperator;
+  }
+  inputHistory.value = inputCalc.value;
+  operator = newOperator;
+}
+
+function numberCalculator(number) {
+  if (inputCalc.value == 0) inputCalc.value = '';
+  inputCalc.value += number;
+  if (operator) {
+    inputHistory.value = inputCalc.value;
+  }
 }
 
 function calculate(num1, num2, operator) {
@@ -51,10 +55,19 @@ function calculate(num1, num2, operator) {
 }
 
 btns.addEventListener('click', (e) => {
-  const target = e.target;
-  const targetType = target.dataset.type;
-  if(target.tagName !== 'BUTTON') return;
+  const button = e.target;
+  const buttonType = button.dataset.type;
 
-  getMapKey[targetType](target);
+  if (button.tagName !== 'BUTTON') return;
+
+  const operationMap = {
+    reset: resetCalculator,
+    equal: equalCalculator,
+    operator: () => operatorCalculator(button.dataset.operator),
+    number: () => numberCalculator(button.dataset.number),
+  };
+
+  const operation = operationMap[buttonType];
+
+  operation();
 });
-
